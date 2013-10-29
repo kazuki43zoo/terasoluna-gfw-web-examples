61066927b4f214b923565756070961bbba04c8eb
kazuki43zoo <sikuzak13@gmail.com>
2013-10-30 08:49:40 +0900

    modified example of pagination(unnecessary import removed).

--- src/main/java/org/terasoluna/gfw/examples/pagination/app/PaginationController.java
+++ src/main/java/org/terasoluna/gfw/examples/pagination/app/PaginationController.java
@@ -4,7 +4,6 @@ import javax.inject.Inject;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
