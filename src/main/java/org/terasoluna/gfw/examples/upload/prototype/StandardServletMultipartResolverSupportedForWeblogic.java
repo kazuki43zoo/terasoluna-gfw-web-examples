9dfb640da2a9bb8bcaa69a8ccd5fbe446d727f10
kazuki43zoo <sikuzak13@gmail.com>
2013-11-09 04:56:38 +0900

    added prototype module of file upload for weblogic supported.

---
+++ src/main/java/org/terasoluna/gfw/examples/upload/prototype/StandardServletMultipartResolverSupportedForWeblogic.java
@@ -0,0 +1,41 @@
+package org.terasoluna.gfw.examples.upload.prototype;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+
+public class StandardServletMultipartResolverSupportedForWeblogic extends StandardServletMultipartResolver {
+
+    @Override
+    public MultipartHttpServletRequest resolveMultipart(final HttpServletRequest request) throws MultipartException {
+        return new StandardMultipartHttpServletRequest(request) {
+            public Enumeration<String> getParameterNames() {
+                List<String> simpleParamaterNames = new ArrayList<String>();
+                try {
+                    for (Part part : request.getParts()) {
+                        if (request.getParameter(part.getName()) != null) {
+                            simpleParamaterNames.add(part.getName());
+                        }
+                    }
+                } catch (IOException e) {
+                    throw new IllegalArgumentException(e);
+                } catch (ServletException e) {
+                    throw new IllegalArgumentException(e);
+                }
+                return Collections.enumeration(simpleParamaterNames);
+            }
+
+        };
+    }
+}
